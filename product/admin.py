7bde9a2c69c43c7d86f0831f569a8382c3d44113
meltemyerli <yerli.meltem16@gmail.com>
2020-03-27 20:10:29 +0300

    Proje Ilk Upload

---
+++ product/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+
+# Register your models here.
+from product.models import Category, Product, Images
+
+class ProductImageInline(admin.TabularInline):
+    model = Images
+    extra = 5
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+    list_filter = ['status']
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'price', 'image_tag', 'status']
+    list_filter = ['status', 'category']
+    inlines = [ProductImageInline]
+    readonly_fields = ('image_tag',)
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['title', 'product', 'image_tag']
+    readonly_fields = ('image_tag',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Images, ImagesAdmin)
